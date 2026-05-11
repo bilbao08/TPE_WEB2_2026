38ea585c8d546014c7c0a6b7a9b4f12cb94c65dc
francisco bilbao <francisco.bilbao30@gmail.com>
2026-05-11 13:57:48 -0300

    avance de tp (A)

---
+++ app/controllers/partidocontroller.php
@@ -0,0 +1,103 @@
+<?php
+
+require_once './app/models/partido.model.php';
+require_once './app/views/partido.view.php';
+
+class PartidoController {
+
+    private $model;
+    private $view;
+
+    public function __construct() {
+
+        $this->model = new PartidoModel();
+        $this->view = new PartidoView();
+    }
+
+    // mostrar todos los partidos
+    public function showPartidos() {
+
+        // pide datos al model
+        $partidos = $this->model->getPartidos();
+
+        // manda datos a la view
+        $this->view->showPartidos($partidos);
+    }
+
+    // mostrar un partido
+    function showPartido() {
+        $partido = $this->model->getPartido();
+        $this->view->showPartido($partido);
+        }
+    
+    function showPartido($id) {
+        $partido = $this->model->getPartido($id);
+        if (!$partido) {
+            $this->view->showError(" No se encontró el partido con ID $id");
+            return;
+        }
+        $this->view->showPartido($partido);
+    }
+
+    function showAgregarPartido() {
+        $this->verificarSesion();
+        $partidos = $this->model->getEstadios();
+        $this->view->showAgregarPartidos($estadios);
+    }
+
+    function addPartido() {
+        $this->verificarSesion();
+        if (!empty($_POST['equipo_local']) && !empty($_POST['equipo_visitante']) && !empty($_POST['fecha']) && !empty($_POST['id_estadio'])) {
+            $equipo_local = $_POST['equipo_local'];
+            $equipo_visitante = $_POST['equipo_visitante'];
+            $fecha = $_POST['fecha'];
+            $id_estadio = $_POST['id_estadio'];
+
+            $this->model->addPartido($equipo_local, $equipo_visitante, $fecha, $id_estadio);
+            header("Location: " . BASE_URL . "partidos");
+        } else {
+            $this->view->showError("Todos los campos son obligatorios");
+        }
+    }
+
+
+    function eliminarPartido($id) {
+        $this->verificarSesion();
+        $partido = $this->model->getPartido($id);
+        if (!$partido) {
+            return $this->view->showError("No existe el partido con el id=$id");
+        }
+        $this->model->eliminarPartido($id);
+        header("Location: " . BASE_URL . "partidos");
+    }
+
+
+    function showeditarPartido($id) {
+        $this->verificarSesion();
+        $partido = $this->model->getPartido($id);
+        $estadios = $this->model->getEstadios();
+        if (!$partido) {
+            $this->view->showError(" No se encontró el partido con ID $id");
+            return;
+        }
+        
+        $this->view->showEditarPartidos($partido, $estadios);
+    }
+    
+    function editarPartido($id) {
+        $this->verificarSesion();
+        $partidoactual = $this->model->getPartido($id);
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $equipo_local = $_POST['equipo_local'] ?? $partidoactual->equipo_local;
+            $equipo_visitante = $_POST['equipo_visitante'] ?? $partidoactual->equipo_visitante;
+            $fecha = $_POST['fecha'] ?? $partidoactual->fecha;
+            $id_estadio = $_POST['id_estadio'] ?? $partidoactual->id_estadio;
+
+            $this->model->editarPartido($id, $equipo_local, $equipo_visitante, $fecha, $id_estadio);
+            header("Location: " . BASE_URL . "partidos");
+        } else {
+            echo "No se recibieron datos para actualizar el partido.";
+        }
+    }
+}
---
+++ router/router.php
@@ -0,0 +1,104 @@
+<?php
+
+require_once './app/controllers/partido.controller.php';
+require_once './app/controllers/estadio.controller.php';
+require_once './app/controllers/user.controller.php';
+
+$partidoController = new PartidoController();
+$estadioController = new EstadioController();
+$userController = new UserController();
+
+session_start();
+
+// base_url para redirecciones y base tag
+define('BASE_URL', '//' . $_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']) . '/');
+
+//verificacion de session
+function checkSession() {
+    if (!isset($_SESSION['usuario'])) {
+        header("Location: " . BASE_URL . "usuarios/login");
+        die();
+    }
+}
+// accion por defecto si no se envia ninguna
+$action = 'partidos';
+
+if (!empty($_GET['action'])) {
+    $action = $_GET['action'];
+}
+
+// parsea la accion para separar accion real de parametros
+$params = explode('/', $action);
+
+// PARTIDOS
+
+if ($params[0] === "partidos") {
+    if (!isset($params[1])) {//listado de partidos
+        $partidoController->showPartidos();
+        exit; //sino seguia ejecutando
+    }
+    switch ($params[1]) {
+        case 'partido':
+            if (isset($params[2])) {
+                $partidoController->showPartido($params[2]);
+            } else {
+                echo "ID de partido no especificado.";
+            }
+        break;
+        case 'crear':
+            checkSession();
+            $partidoController->addPartido();
+        break;
+        case 'agregar':
+            checkSession();
+            $partidoController->showAgregarPartido();
+        break;
+        case 'editar':
+            checkSession();
+            if (isset($params[2])) {
+                $partidoController->showEditarPartido($params[2]);
+            } else {
+                echo "No se recibió el ID del partido.";
+            }
+        break;
+        case 'actualizar':
+            checkSession();
+            if (isset($params[2])) {
+                $partidoController->editarPartido($params[2]);
+            } else if (isset($_POST['id'])) {
+                $partidoController->editarPartido($_POST['id']);
+            } else {
+                echo "No se recibió el ID del partido para actualizar.";
+            }
+        break;
+        case 'eliminar':
+            checkSession();
+            if (isset($params[2])) {
+                $partidoController->eliminarPartido($params[2]);
+            } else {
+                echo "No se recibió el ID del partido para eliminar.";
+            }
+        break;
+        default:
+            echo "404 Page Not Found";
+        break;
+    }
+}
+
+// USUARIOS
+if ($params[0] === "usuarios") {
+    switch ($params[1]) {
+        case 'login':
+            $userController->showIniciarSesion();
+            break;
+        case 'ingresar':
+            $userController->iniciarSesion();
+            break;
+        case 'logout':
+            $userController->cerrarSesion();
+            break;
+        default:
+            echo "404 Page Not Found";
+            break;
+    }
+}
